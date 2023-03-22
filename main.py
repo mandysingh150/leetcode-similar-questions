c96e25431cc0b3f152c97fa680dfb22ee71a3870
mandysingh150 <mandysingh150@gmail.com>
2023-03-22 19:53:59 +0530

    solution link and solution content function added

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import bs4
 import colorama
 import requests
 from colorama import Back, Fore
-from ebooklib import epub
+# from ebooklib import epub
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
@@ -22,7 +22,9 @@ colorama.init(autoreset=True)
 # Setup Selenium Webdriver
 CHROMEDRIVER_PATH = r"./driver/chromedriver.exe"
 options = Options()
-options.headless = True
+# options.headless = False
+# options.page_load_strategy = 'normal'
+# options.add_argument("start-maximized")
 # Disable Warning, Error and Info logs
 # Show only fatal errors
 options.add_argument("--log-level=3")
@@ -46,8 +48,8 @@ def download(problem_num, url, title, solution_slug):
         # For obtaining the problem description
         ####################################################################
         driver.get(url)
-        # Wait 30 secs or until div with class '_1l1MA' appears
-        element = WebDriverWait(driver, 30).until(
+        # Wait 10 secs or until div with class '_1l1MA' appears
+        element = WebDriverWait(driver, 10).until(
             EC.visibility_of_element_located((By.CLASS_NAME, "_1l1MA"))
         )
         # Get current tab page source
@@ -57,7 +59,8 @@ def download(problem_num, url, title, solution_slug):
         problem_statement_examples_contraints = ''
         for val in tt:
             problem_statement_examples_contraints += f'{val.lstrip().rstrip()} '
-        print(f'problem = {problem_statement_examples_contraints}')
+        # print(Fore.BLACK + Back.CYAN + 'PROBLEM/QUESTION = ' + Back.YELLOW + f'{problem_statement_examples_contraints}')
+        print('\t==> problem description')
 
         # Construct HTML
         # title_decorator = '*' * n
@@ -69,33 +72,52 @@ def download(problem_num, url, title, solution_slug):
         ####################################################################
         # For obtaining the most voted solution link of the problem
         ####################################################################
+        # time.sleep(0.5)
         url = url + '/solutions/?orderBy=most_votes'
         # import validators
         # valid=validators.url('https://www.codespeedy.com/')
         # print(f'valid = {valid}')
         driver.get(url)
-        # Wait 30 secs or until div with class '_1l1MA' appears
-        element = WebDriverWait(driver, 4).until(
-            EC.visibility_of_element_located((By.CLASS_NAME, "transition-[background] duration-500"))
+        # print(f'-- before WebDriverWait --')
+        # Wait 10 secs or until div with class 'transition-[background] duration-500' appears
+        element = WebDriverWait(driver, 10).until(
+            # EC.visibility_of_element_located((By.CLASS_NAME, "transition-[background]"))
+            lambda x: x.find_element(By.XPATH, "//*[@class='relative flex w-full gap-4 px-5 py-3 transition-[background] duration-500']")
+            # lambda x: x.find_element(By.CSS_SELECTOR, ".relative.flex.w-full.gap-4.px-5.py-3.transition-[background].duration-500")
+            # EC.presence_of_element_located((By.CSS_SELECTOR, ".relative.flex.w-full.gap-4.px-5.py-3.transition-[background].duration-500"))
         )
+        # .until(
+        #     EC.visibility_of_element_located((By.CLASS_NAME, "relative flex w-full gap-4 px-5 py-3 transition-[background] duration-500"))
+        #     # EC.visibility_of_element_located((By.CLASS_NAME, "relative flex w-full gap-4 px-5 py-3 transition-[background] duration-5ṇ00"))
+        #     # EC.visibility_of_element_located((By.CLASS_NAME, "transition-[background] duration-500"))
+        # )
+        # print(Fore.BLACK + Back.CYAN + 'type of object = ' + Back.YELLOW + f'{type(element)}')
         # Get current tab page source
         html = driver.page_source
+        # print(f'-- after WebDriverWait --')
+        # with open('most_voted_sol.html', 'w') as ff:
+        #     ff.writelines(str(driver.page_source))
         soup = bs4.BeautifulSoup(html, "html.parser")
         tt = soup.find("div", {"class": "relative flex w-full gap-4 px-5 py-3 transition-[background] duration-500"})
         solution_slug = tt.div.div.div.a['href']
         solution_page_link = f'https://leetcode.com{solution_slug}'
-        print(f'solution_link = {solution_page_link}')
+        print(f'\t==> solution_link = {solution_page_link}')
+        # print(Fore.BLACK + Back.CYAN + 'solution_link = ' + Back.YELLOW + f'{solution_page_link}')
+        # print('\t==> most voted solution')
+
 
 
 
         ####################################################################
         # For obtaining the content of the most voted solution of the problem
         ####################################################################
+        # time.sleep(0.5)
         url = solution_page_link
         driver.get(url)
-        # Wait 30 secs or until div with class '_1l1MA' appears
-        element = WebDriverWait(driver, 30).until(
-            EC.visibility_of_element_located((By.CLASS_NAME, "_16yfq _2YoR3"))
+        # Wait 10 secs or until div with class '_16yfq _2YoR3' appears
+        element = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "._16yfq._2YoR3"))
+            # EC.visibility_of_element_located((By.CLASS_NAME, "_16yfq _2YoR3"))
         )
         # Get current tab page source
         html = driver.page_source
@@ -106,15 +128,16 @@ def download(problem_num, url, title, solution_slug):
         tt = soup.find("div", {"class": "mb-6 rounded-lg px-3 py-2.5 font-menlo text-sm bg-fill-3 dark:bg-dark-fill-3"})
         txt2 = tt.get_text().splitlines()
 
-        print(txt1)
-        print(txt2)
+        # print(txt1)
+        # print(txt2)
 
         solution_content = ''
         for text in txt1:
             if text in txt2 and text.lstrip().rstrip() != '':
                 break
             solution_content += f'{text}\n'
-        print(f'content = {solution_content}')
+        # print(f'content = {solution_content}')
+        print('\t==> most voted solution content')
 
 
         # Append Contents to a HTML file
@@ -128,11 +151,10 @@ def download(problem_num, url, title, solution_slug):
 
         # Write List of chapters to pickle file
         # dump_chapters_to_file(chapters)
-        # Update upto which the problem is downloaded
-        update_tracker('track.conf', problem_num)
-        print(Fore.BLACK + Back.GREEN + f"Writing problem num " + Back.YELLOW + f" {problem_num} ")
-        # + Back.GREEN + " with url " + Back.YELLOW + f" {url} " )
-        print(Fore.BLACK + Back.GREEN + " successfull ")
+        # # Update upto which the problem is downloaded
+        # update_tracker('track.conf', problem_num)
+        # print(Fore.BLACK + Back.GREEN + f"Writing problem num " + Back.YELLOW + f" {problem_num} ==>", end='  ')
+        # print(Fore.BLACK + Back.GREEN + " SUCCESSFUL ")
         # print(f"Writing problem num {problem_num} with url {url} successfull")
 
 
@@ -141,6 +163,7 @@ def download(problem_num, url, title, solution_slug):
     except Exception as e:
         print(Back.RED + f" Failed Writing!!  {e} ")
         driver.quit()
+        exit(0)
         return '', '', ''
 
 
@@ -159,9 +182,9 @@ def main():
     algorithms_problems_json = requests.get(ALGORITHMS_ENDPOINT_URL).content
     algorithms_problems_json = json.loads(algorithms_problems_json)
 
-    styles_str = "<style>pre{white-space:pre-wrap;background:#f7f9fa;padding:10px 15px;color:#263238;line-height:1.6;font-size:13px;border-radius:3px margin-top: 0;margin-bottom:1em;overflow:auto}b,strong{font-weight:bolder}#title{font-size:16px;color:#212121;font-weight:600;margin-bottom:10px}hr{height:10px;border:0;box-shadow:0 10px 10px -10px #8c8b8b inset}</style>"
-    with open("out.html", "ab") as f:
-            f.write(styles_str.encode(encoding="utf-8"))
+    # styles_str = "<style>pre{white-space:pre-wrap;background:#f7f9fa;padding:10px 15px;color:#263238;line-height:1.6;font-size:13px;border-radius:3px margin-top: 0;margin-bottom:1em;overflow:auto}b,strong{font-weight:bolder}#title{font-size:16px;color:#212121;font-weight:600;margin-bottom:10px}hr{height:10px;border:0;box-shadow:0 10px 10px -10px #8c8b8b inset}</style>"
+    # with open("out.html", "ab") as f:
+        # f.write(styles_str.encode(encoding="utf-8"))
 
     # List to store question_title_slug
     links = []
@@ -176,38 +199,54 @@ def main():
             links.append((question__title_slug, difficulty, frontend_question_id, question__title, question__article__slug))
 
     # Sort by difficulty follwed by problem id in ascending order
-    links = sorted(links, key=lambda x: (x[1], x[2]))
+    links = sorted(links, key=lambda x: x[2])
 
     try: 
-        for i in range(completed_upto + 1, len(links)):
-            question__title_slug, _ , frontend_question_id, question__title, question__article__slug = links[i]
+        for problem_num in range(completed_upto + 1, len(links)):
+            question__title_slug, _ , frontend_question_id, question__title, question__article__slug = links[problem_num]
             url = ALGORITHMS_BASE_URL + question__title_slug
             title = f"{frontend_question_id}. {question__title}"
 
             # Download each file as html and write chapter to chapters.pickle
-            problem_statement_examples_contraints, solution_page_link, solution_content = download(i, url , title, question__article__slug)
+            problem_statement_examples_contraints, solution_page_link, solution_content = download(problem_num+1, url , title, question__article__slug)
 
             if problem_statement_examples_contraints != '':
+                row_item = [frontend_question_id, question__title, problem_statement_examples_contraints, solution_page_link, solution_content]
+
                 with open('data.csv', 'a') as fp:
-                    fp.write(f'{frontend_question_id},{question__title},{problem_statement_examples_contraints},{solution_page_link},{solution_content}')
+                    print(Fore.BLACK + Back.CYAN + f"\tWriting problem num " + Back.YELLOW + f' {problem_num+1} ' + Back.CYAN  + "  ==>  ", end='')
+                    
+                    # fp.write(f'{frontend_question_id},{question__title},{problem_statement_examples_contraints},{solution_page_link},{solution_content}')
+                    # for item in row_item:
+                    #     item = '`'.join(item.split(','))
+                    
+                    row_item = ['`'.join(str(x).split(',')) for x in row_item]
+                    # print(f'row_item = {row_item}')
+
+                    fp.write(','.join(row_item))
+                    
+                    print(Fore.BLACK + Back.GREEN + " SUCCESSFUL ")
+
+                    # Update upto which the problem is downloaded
+                    # update_tracker('track.conf', problem_num)
 
             # Sleep for 20 secs for each problem and 2 minns after every 30 problems
-            if (i+1) % 30 == 0:
+            if (problem_num+1) % 30 == 0:
                 print(f"Sleeping 120 secs\n")
                 time.sleep(120)
             else:
-                print(f"Sleeping 20 secs\n")
-                time.sleep(5)
+                print(f"Sleeping 25 secs\n")
+                time.sleep(25)
 
     finally:
         # Close the browser after download
         driver.quit()
     
-    try:
-        epub_writer.write("Leetcode Questions.epub", "Leetcode Questions", "Anonymous", chapters)
-        print(Back.GREEN + "All operations successful")
-    except Exception as e:
-        print(Back.RED + f"Error making epub {e}")
+    # try:
+    #     epub_writer.write("Leetcode Questions.epub", "Leetcode Questions", "Anonymous", chapters)
+    #     print(Back.GREEN + "All operations successful")
+    # except Exception as e:
+    #     print(Back.RED + f"Error making epub {e}")
     
 
 
